d31daf1aede48d6ed4d3442864f41ab0dc30dee4
Justin <justindchan@gmail.com>
2019-09-04 23:08:21 -0700

    proxy server serving up bundled app with team components rendering.

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const PORT = 3000
 
 app.use(express.static('./public')); 
 
+// app.get('', (req, res) => {
+//   axios.get('serivceURL')
+//     .then()
+// });
+
 app.listen(PORT, () => console.log(`HO YA ON ${PORT}!`))
 
